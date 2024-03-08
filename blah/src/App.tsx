94a13b95b21ae289b6902d7b982d50492492195e
jacobmillett <jakemillett1@gmail.com>
2024-03-08 10:41:24 -0700

    Finished Mission 9

--- blah/src/App.tsx
+++ blah/src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import jsonData from "./CollegeBasketballTeams.json";
 
+interface CBBTeam {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function Welcome() {
+  return (
+    <div>
+      <h1>NCAA Basketball Team's</h1>{" "}
+      <h2>
+        This website is a list of all of the NCAA Basketball team, their mascots
+        and their location.
+      </h2>
+      ;
+    </div>
+  );
+}
+
+class Team extends React.Component<CBBTeam> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div
+        style={{ border: "2px solid #000", padding: "20px", width: "200px" }}
+      >
+        <h2>{oneTeam.school}</h2>
+        <h4>{oneTeam.name}</h4>
+        <h4>
+          {oneTeam.city}, {oneTeam.state}
+        </h4>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
+      {jsonData.teams.map((teamNum: CBBTeam) => (
+        <Team key={teamNum.school} {...teamNum} />
+      ))}
+    </div>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
